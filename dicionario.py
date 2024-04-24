fe0c3d4a0fa9b7009242febbe3e8e31b990106ae
AbigailAmira <166626097+AbigailAmira@users.noreply.github.com>
2024-04-24 07:55:47 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,25 @@
+ListaNomes = []
+
+
+ListaNomes.append("Edson")
+ListaNomes.append("Tamires")
+ListaNomes.append("Sophia")
+
+print(ListaNomes)
+print(ListaNomes[2])
+
+for nome in ListaNomes:
+    print(nome)
+
+#DICIONARIOS 
+primeiroDicionario = {}
+
+primeiroDicionario['nome'] = "Edson"
+primeiroDicionario['tipo'] = 'professor'
+primeiroDicionario['numero'] = 9
+print(primeiroDicionario )
+print(primeiroDicionario ["tipo"])
+
+#Anatomia 
+#lista[indice] = valor
+#dicionario[chaves] = valor
